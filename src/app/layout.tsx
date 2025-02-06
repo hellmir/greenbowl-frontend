b284160e14ff009cc3ed284925866a083fe29f91
pjc0179 <pjc0179@gmail.com>
2025-02-07 07:25:34 +0900

    feat:"글로벌 폰트 적용"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 import { Navigation } from "@/components/navigation";
+import { Header } from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const myFont = localFont({
+  src: [
+    {
+      path: "../fonts/SpoqaHanSansNeo-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SpoqaHanSansNeo-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/SpoqaHanSansNeo-Light.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -25,12 +36,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="w-full max-w-sm mx-auto border h-screen relative">
-          {children}
-          <Navigation />
+      <body className={`${myFont.className} antialiased`}>
+        <div className="flex justify-center items-center h-screen w-screen">
+          <div className="w-full max-w-[599px] h-full  mx-auto relative border-2 border-foundation-primary bg-foundation-quarternary">
+            <Header />
+            <div className="mt-[54px] pl-4 pr-4">{children}</div>
+            <Navigation />
+          </div>
         </div>
       </body>
     </html>
