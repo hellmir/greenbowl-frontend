ed1a2f4161483eec17cf66acbab50d3040170206
pjc0179 <pjc0179@gmail.com>
2025-02-13 09:26:52 +0900

    refactor:"내재료모달 버튼 수정"

--- src/app/myFridge/myIngredient/_source/components/Storage/modal/SelectConfigTypeModal.tsx
+++ src/app/myFridge/myIngredient/_source/components/Storage/modal/SelectConfigTypeModal.tsx
@@ -8,12 +8,20 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import useEditIngredients from "@/store/editIngredientsStore";
 import useIngredientConfigState from "@/store/ingredientConfigStore";
+import { Config } from "../MyIngredients";
+import Link from "next/link";
+import route from "@/constants/route";
 
-const ModalBtn = () => {
-  const changeConfigState = useIngredientConfigState(
-    (state) => state.changeConfigState
-  );
+const SelectConfigTypeModal = () => {
+  const { changeConfigState } = useIngredientConfigState();
+  const { allClear } = useEditIngredients();
+
+  const handleClick = (s: Config) => {
+    changeConfigState(s);
+    allClear();
+  };
 
   return (
     <Dialog>
@@ -22,17 +30,19 @@ const ModalBtn = () => {
       </DialogTrigger>
       <DialogContent className=" bg-inherit border-none rounded-md mx-auto top-3/4 h-0 max-w-[599px] w-screen pr-4 pl-4">
         <DialogTitle className=" hidden"></DialogTitle>
-        <DialogClose onClick={() => changeConfigState("none")}>
-          <div className="bg-white rounded-lg h-14  border-border-border flex items-center justify-center">
-            추가하기
-          </div>
+        <DialogClose onClick={() => handleClick("none")}>
+          <Link href={route.myFridge.addIngredient}>
+            <div className="bg-white rounded-lg h-14  border-border-border flex items-center justify-center">
+              추가하기
+            </div>
+          </Link>
         </DialogClose>
-        <DialogClose onClick={() => changeConfigState("edit")}>
+        <DialogClose onClick={() => handleClick("edit")}>
           <div className="bg-white rounded-lg h-14 flex items-center justify-center">
             수정하기
           </div>
         </DialogClose>
-        <DialogClose onClick={() => changeConfigState("delete")}>
+        <DialogClose onClick={() => handleClick("delete")}>
           <div className="bg-white rounded-lg h-14 flex items-center justify-center">
             삭제하기
           </div>
@@ -42,4 +52,4 @@ const ModalBtn = () => {
   );
 };
 
-export default ModalBtn;
+export default SelectConfigTypeModal;
