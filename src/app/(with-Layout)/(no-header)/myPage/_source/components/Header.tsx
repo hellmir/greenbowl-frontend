0556c9e83287c0e163b1a65dace6f10c0718e32a
pjc0179 <pjc0179@gmail.com>
2025-02-24 01:20:19 +0900

    feat:"마이페이지 탭 콘탠츠 부분 제외 구현"

--- src/app/(with-Layout)/(no-header)/myPage/_source/components/Header.tsx
+++ src/app/(with-Layout)/(no-header)/myPage/_source/components/Header.tsx
@@ -2,10 +2,10 @@ import SettingIcon from "@/components/icons/SettingIcon";
 
 const MyPageHeader = () => {
   return (
-    <header className=" h-[3.375rem] w-full max-w-[35.5rem] fixed z-30 bg-foundation-secondary">
+    <header className="top-0 h-[3.375rem] w-full max-w-[35.5rem] fixed z-30 bg-foundation-secondary">
       <div className=" w-full h-full flex items-center justify-center relative">
         <p className=" heading-m text-content-secondary">마이 페이지</p>
-        <div className="absolute">
+        <div className="absolute right-0">
           <SettingIcon fill="content-tertiary" />
         </div>
       </div>
--- src/app/(with-Layout)/(no-header)/myPage/page.tsx
+++ src/app/(with-Layout)/(no-header)/myPage/page.tsx
@@ -1,9 +1,21 @@
 import MyPageHeader from "./_source/components/Header";
+import TabsContainer from "./_source/components/Tabs";
+import UserProfile from "./_source/components/UserProfile";
 
-const page = () => {
+interface Props {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+const page = async ({ searchParams }: Props) => {
+  const params = await searchParams;
+  const tabName = params.tabName ? (params.tabName as string) : "recipe";
+
   return (
     <div>
       <MyPageHeader />
+      <div className="mt-[3.275rem]">
+        <UserProfile />
+        <TabsContainer tabName={tabName} />
+      </div>
     </div>
   );
 };
