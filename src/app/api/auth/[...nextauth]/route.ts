66956893a193b57fd5a1d27821f06f67bbaacf4b
pjc0179 <pjc0179@gmail.com>
2025-02-23 19:20:27 +0900

    refactor:"클라이언트 id 및 secret 보안 문제로 next public 제거거"

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
-console.log(process.env.NEXT_PUBLIC_OAUTH_KAKAO_CLIENT_ID);
+
 const handler = NextAuth({
   providers: [
     KakaoProvider({
-      clientId: process.env.NEXT_PUBLIC_OAUTH_KAKAO_CLIENT_ID as string,
-      clientSecret: process.env.NEXT_PUBLIC_OAUTH_KAKAO_CLIENT_SECRET as string,
+      clientId: process.env.OAUTH_KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.OAUTH_KAKAO_CLIENT_SECRET as string,
     }),
   ],
   pages: {
