dd6977806102a68c40afc0de4d92ec05f0053094
pjc0179 <pjc0179@gmail.com>
2025-02-28 11:45:12 +0900

    feat:"로그인쪽 그룹으로 분리 및 empty 이미지 추가와 안쓰는 파일들 제거"

--- src/app/(no-Layout)/(login)/_source/components/SocialBtns.tsx
+++ src/app/(no-Layout)/(login)/_source/components/SocialBtns.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import KakaoLoginSvg from "@/components/svg/Kakao";
 import GoogleLoginSvg from "@/components/svg/Google";
 import route from "@/constants/route";
 
 const SocialLoginButtons = () => {
   const router = useRouter();
+  const pathName = usePathname();
+
+  const isOnboardingLogin = pathName.includes("onboarding");
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 ">
       <button
         onClick={async () => await signIn("kakao")}
         className="h-14 rounded-xl bg-[#FDE500] text-black py-3 px-5 flex items-center justify-center"
@@ -27,12 +30,14 @@ const SocialLoginButtons = () => {
         <p className="label-l ml-2">구글로 로그인</p>
       </button>
 
-      <button
-        onClick={() => router.push(route.recipe.root)}
-        className="h-14 rounded-xl bg-white text-black py-3 px-5 flex items-center justify-center border border-[#E6E6E6]"
-      >
-        <p className="label-l ml-2">게스트로 둘러 보기</p>
-      </button>
+      {isOnboardingLogin && (
+        <button
+          onClick={() => router.push(route.recipe.root)}
+          className="h-14 rounded-xl bg-white text-black py-3 px-5 flex items-center justify-center border border-[#E6E6E6]"
+        >
+          <p className="label-l ml-2">게스트로 둘러 보기</p>
+        </button>
+      )}
     </div>
   );
 };
--- src/app/(with-Layout)/(with-header)/fridge/_source/components/buttons/Delete.tsx
+++
@@ -1,9 +0,0 @@
-import { deleteIngredients } from "../../actions/ingredient";
-
-const Delete = ({ id }: { id: number[] }) => {
-  const handleClick = () => deleteIngredients(id);
-
-  return <button type="button" onClick={handleClick}></button>;
-};
-
-export default Delete;
