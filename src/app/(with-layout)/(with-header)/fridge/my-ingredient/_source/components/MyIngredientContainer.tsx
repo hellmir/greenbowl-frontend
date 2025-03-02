205b11fae26d5006ee8a235cfe1cf554ba30e949
pjc0179 <pjc0179@gmail.com>
2025-03-02 21:13:44 +0900

    feat:"냉장고 재료 에러 핸들링링"

--- src/app/(with-layout)/(with-header)/fridge/my-ingredient/_source/components/MyIngredientContainer.tsx
+++ src/app/(with-layout)/(with-header)/fridge/my-ingredient/_source/components/MyIngredientContainer.tsx
@@ -4,17 +4,23 @@ import Storage from "./Storage";
 import { getFridgeIngredients } from "../actions/fridgeIngredient";
 
 const MyIngredientContainer = async () => {
-  const ingredients = await getFridgeIngredients();
-  console.log(ingredients);
-  return (
-    <>
-      {ingredients.length === 0 ? (
-        <Empty />
-      ) : (
-        <Storage ingredients={ingredients} />
-      )}
-    </>
-  );
+  try {
+    const ingredients = await getFridgeIngredients();
+
+    return (
+      <>
+        {ingredients.length === 0 ? (
+          <Empty />
+        ) : (
+          <Storage ingredients={ingredients} />
+        )}
+      </>
+    );
+  } catch {
+    <div className="h-full w-full flex items-center justify-center">
+      냉장고 재료를 가져오는 도중 에러가 발생했습니다.
+    </div>;
+  }
 };
 
 export default MyIngredientContainer;
