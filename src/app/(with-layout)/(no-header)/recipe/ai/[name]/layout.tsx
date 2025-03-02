810ad39b4b22e55e207b6ca60d704e49d7d515e9
pjc0179 <pjc0179@gmail.com>
2025-03-02 21:52:29 +0900

    chore:" ai 추천 레시피 메타태그 변경경"

--- src/app/(with-layout)/(no-header)/recipe/ai/[name]/layout.tsx
+++ src/app/(with-layout)/(no-header)/recipe/ai/[name]/layout.tsx
@@ -1,13 +1,13 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "추천 레시피",
+  title: "AI 추천 레시피",
 };
 
 export default function RecipeLayout({
-                                         children,
-                                     }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return <>{children}</>;
+  return <>{children}</>;
 }
