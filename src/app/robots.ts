ab3b96bfe3959bf316a512674b6ac45e05bd6c2b
pjc0179 <pjc0179@gmail.com>
2025-04-01 16:58:31 +0900

    chore:"robots 수정 "

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -17,7 +17,6 @@ export default function robots(): MetadataRoute.Robots {
           "/fridge/my-ingredient",
           "/fridge/add-ingredient",
           "/recipe/*",
-          "/onboarding"
         ],
       },
     ],
